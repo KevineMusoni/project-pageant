a10fda242b0d7b7e87b2589ab15f87cfac799205
Kevine Musoni <kevinenicky@gmail.com>
2018-12-31 16:53:13 +0300

    profile & projects forms

---
+++ awward/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Profile, Projects
+
+class ProjectsForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['profile','functionality','userinterface']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['profile']
