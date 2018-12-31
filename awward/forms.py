29a6a91f3180c886de612d83205e90fd32d9d595
Kevine Musoni <kevinenicky@gmail.com>
2018-12-31 17:10:29 +0300

    created a serializer file

--- awward/forms.py
+++ awward/forms.py
@@ -10,3 +10,4 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['profile']
+        
---
+++ awward/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Project,Profile
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('title','image','description', 'link','profile','userinterface','functionality')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('photo','profile', 'bio')
