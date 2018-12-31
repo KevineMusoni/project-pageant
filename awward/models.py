a9c2ac1b67a7c4d8b39c58abd58d5569f6311af7
Kevine Musoni <kevinenicky@gmail.com>
2018-12-31 16:11:53 +0300

    added a user profile class & added new files

--- awward/models.py
+++ awward/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+    photo = models.ImageField(upload_to = 'profile/')
+    Profile = models.ForeignKey(User,on_delete=models.CASCADE, null = True)
+    bio = models.TextField(max_length=500)
+
+    def save_profile(self)
+        self.save()
+    def delete_profile(self)
+        self.delete()
